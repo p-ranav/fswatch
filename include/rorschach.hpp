ba56aa430cdded774ef53075ab05769cd37b5c35
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-08-17 21:44:24 -0500

    Ignoring permission denied when building list of files

--- include/rorschach.hpp
+++ include/rorschach.hpp
@@ -5,6 +5,8 @@
 #include <string>
 #include <thread>
 #include <unordered_map>
+#include <regex>
+#include <iostream>
 
 class Rorschach {
 public:
@@ -15,17 +17,24 @@ public:
   std::chrono::duration<int, std::milli> period;
   // Dictionary that maps files with their respective last_write_time timestmaps
   std::unordered_map<std::string, std::filesystem::file_time_type> file_last_write_time_map;
+  // Regex of paths to ignore
+  std::regex ignore_path;
 
   std::function<void (const std::string&)> on_path_created, on_path_modified, on_path_erased;
 
-  Rorschach(const std::string& path, std::chrono::duration<int, std::milli> period) : 
-    running(true), path(path), period(period), file_last_write_time_map({}) {}
+  Rorschach(const std::string& path, std::chrono::duration<int, std::milli> period, const std::regex& ignore_path) : 
+    running(true), path(path), period(period), ignore_path(ignore_path), file_last_write_time_map({}) {}
 
   void watch() {
 
     // Build file map for user-specified path
-    for (auto& file: std::filesystem::recursive_directory_iterator(path)) {
+    for (auto& file: std::filesystem::recursive_directory_iterator(path, std::filesystem::directory_options::skip_permission_denied)) {
+      std::string filename = file.path().string();
       file_last_write_time_map[file.path().string()] = std::filesystem::last_write_time(file);
+       std::smatch match;
+      // Watch this file only if it is not in the ignore list
+      if (std::regex_search(filename, match, ignore_path) && match.size() == 0)
+        file_last_write_time_map[filename] = std::filesystem::last_write_time(file);
     }
 
     // Start watching files in path
@@ -46,15 +55,22 @@ public:
 
       // Check if the file was created or modified
       for (auto& file : std::filesystem::recursive_directory_iterator(path)) {
-        auto current_file_last_write_time = std::filesystem::last_write_time(file);
-        if (!contains(file.path().string())) {
-          file_last_write_time_map[file.path().string()] = current_file_last_write_time;
-          if (on_path_created) on_path_created(file.path().string());
-        }
-        else {
-          if (file_last_write_time_map[file.path().string()] != current_file_last_write_time) {
+
+        std::string filename = file.path().string();
+        std::smatch match;
+        // Watch this file only if it is not in the ignore list
+        if (std::regex_search(filename, match, ignore_path) && match.size() == 0) {
+          std::error_code ec;
+          auto current_file_last_write_time = std::filesystem::last_write_time(file, ec);
+          if (!contains(file.path().string())) {
             file_last_write_time_map[file.path().string()] = current_file_last_write_time;
-            if (on_path_modified) on_path_modified(file.path().string());
+            if (on_path_created) on_path_created(file.path().string());
+          }
+          else {
+            if (file_last_write_time_map[file.path().string()] != current_file_last_write_time) {
+              file_last_write_time_map[file.path().string()] = current_file_last_write_time;
+              if (on_path_modified) on_path_modified(file.path().string());
+            }
           }
         }
       }
