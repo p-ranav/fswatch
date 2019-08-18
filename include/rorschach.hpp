654b9544c219810b6da9153c6b7f27015941f761
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-08-18 14:39:45 -0500

    .on(...) method and clang-format

--- include/rorschach.hpp
+++ include/rorschach.hpp
@@ -1,98 +1,120 @@
 #pragma once
-#include <filesystem>
 #include <chrono>
+#include <filesystem>
 #include <functional>
+#include <iostream>
+#include <regex>
 #include <string>
 #include <thread>
 #include <unordered_map>
-#include <regex>
-#include <iostream>
+#include <variant>
+
+enum class FileStatus { FILE_CREATED, FILE_MODIFIED, FILE_ERASED };
 
 class Rorschach {
 public:
-  // Root directory of the file watcher
-  std::filesystem::path path;
-  // Periodicity of the file watcher, i.e., how often it checks to see if files have changed
-  std::chrono::duration<int, std::milli> period;
-  // Regex of paths to ignore
-  std::regex ignore_path;
-  // Callback functions based on file status
-  std::function<void (const std::filesystem::path&)> on_path_created, on_path_modified, on_path_erased;
+  Rorschach(const std::string &path,
+            std::chrono::duration<int, std::milli> period)
+      : running_(true), path(expand(std::filesystem::path(path))),
+        period(period), lwt_map_({}) {}
 
-  Rorschach(const std::string& path, std::chrono::duration<int, std::milli> period) : 
-    running_(true), path(expand(std::filesystem::path(path))), period(period), lwt_map_({}) {}
+  void ignore(const std::regex &ignore) { ignore_path = ignore; }
 
-  void ignore(const std::regex& ignore) {
-    ignore_path = ignore;
+  void on(const FileStatus &event,
+          const std::function<void(const std::filesystem::path &)> &action) {
+    switch (event) {
+    case FileStatus::FILE_CREATED:
+      on_path_created = action;
+      break;
+    case FileStatus::FILE_MODIFIED:
+      on_path_modified = action;
+      break;
+    case FileStatus::FILE_ERASED:
+      on_path_erased = action;
+      break;
+    }
   }
 
   void watch() {
 
     // Build file map for user-specified path
-    for (auto& file: 
-	   std::filesystem::recursive_directory_iterator(std::filesystem::absolute(path), 
-							 std::filesystem::directory_options::skip_permission_denied)) {
+    for (auto &file : std::filesystem::recursive_directory_iterator(
+             std::filesystem::absolute(path),
+             std::filesystem::directory_options::skip_permission_denied)) {
       if (!should_ignore(file)) {
         std::error_code ec;
-        lwt_map_[file.path().string()] = std::filesystem::last_write_time(file, ec);
+        lwt_map_[file.path().string()] =
+            std::filesystem::last_write_time(file, ec);
       }
     }
 
     // Start watching files in path
-    while(running_) {
+    while (running_) {
       std::this_thread::sleep_for(period);
       auto it = lwt_map_.begin();
 
       // Check if the file was erased
       while (it != lwt_map_.end()) {
         if (!std::filesystem::exists(it->first)) {
-          if (on_path_erased) on_path_erased(std::filesystem::path(it->first));
+          if (on_path_erased)
+            on_path_erased(std::filesystem::path(it->first));
           it = lwt_map_.erase(it);
-        }
-        else {
+        } else {
           it++;
         }
       }
 
       // Check if the file was created or modified
-      for (auto& file: 
-	     std::filesystem::recursive_directory_iterator(std::filesystem::absolute(path), 
-							   std::filesystem::directory_options::skip_permission_denied)) {
+      for (auto &file : std::filesystem::recursive_directory_iterator(
+               std::filesystem::absolute(path),
+               std::filesystem::directory_options::skip_permission_denied)) {
         std::string filename = file.path().string();
         // Watch this file only if it is not in the ignore list
         if (!should_ignore(file)) {
           std::error_code ec;
-          auto current_file_last_write_time = std::filesystem::last_write_time(file, ec);
+          auto current_file_last_write_time =
+              std::filesystem::last_write_time(file, ec);
           if (!contains(file.path().string())) {
             lwt_map_[file.path().string()] = current_file_last_write_time;
-            if (on_path_created) on_path_created(file.path());
-          }
-          else {
-            if (lwt_map_[file.path().string()] != current_file_last_write_time) {
+            if (on_path_created)
+              on_path_created(file.path());
+          } else {
+            if (lwt_map_[file.path().string()] !=
+                current_file_last_write_time) {
               lwt_map_[file.path().string()] = current_file_last_write_time;
-              if (on_path_modified) on_path_modified(file.path());
+              if (on_path_modified)
+                on_path_modified(file.path());
             }
           }
         }
       }
-
     }
   }
 
 private:
+  // Root directory of the file watcher
+  std::filesystem::path path;
+  // Periodicity of the file watcher, i.e., how often it checks to see if files
+  // have changed
+  std::chrono::duration<int, std::milli> period;
+  // Regex of paths to ignore
+  std::regex ignore_path;
+  // Callback functions based on file status
+  std::function<void(const std::filesystem::path &)> on_path_created,
+      on_path_modified, on_path_erased;
   // Manage status of file watcher
   bool running_ = true;
   // Dictionary that maps files with their respective last_write_time timestmaps
   std::unordered_map<std::string, std::filesystem::file_time_type> lwt_map_;
 
-  bool contains(const std::string& file) {
+  bool contains(const std::string &file) {
     return lwt_map_.find(file) != lwt_map_.end();
   }
 
   std::filesystem::path expand(std::filesystem::path in) {
-    const char * home = getenv("HOME");
+    const char *home = getenv("HOME");
     if (!home)
-      throw std::invalid_argument ("HOME environment variable not set.");
+      throw std::invalid_argument("HOME environment variable not set.");
 
     std::string result = in.c_str();
     if (result.length() > 0 && result[0] == '~') {
@@ -102,12 +124,11 @@ private:
     return in;
   }
 
-  bool should_ignore(const std::filesystem::directory_entry& path) {
+  bool should_ignore(const std::filesystem::directory_entry &path) {
     std::string filename = path.path().string();
     if (!std::regex_search(filename, ignore_path))
       return false;
     else
       return true;
   }
-
 };
--- test/main.cpp
+++ test/main.cpp
@@ -1,26 +1,25 @@
-#include <rorschach.hpp>
 #include <iostream>
+#include <rorschach.hpp>
 
 int main() {
   auto rorschach = Rorschach("", std::chrono::milliseconds(500));
   rorschach.ignore(std::regex(".*\\.ini$")); // Ignore .ini files
 
-  rorschach.on_path_created = [](auto& path) { 
-    std::cout << "Path created: " << path << std::endl; 
-  };
+  rorschach.on(FileStatus::FILE_CREATED, [](auto &path) {
+    std::cout << "Path created: " << path << std::endl;
+  });
 
-  rorschach.on_path_modified = [](auto& path) {
+  rorschach.on(FileStatus::FILE_MODIFIED, [](auto &path) {
     std::cout << "Path modified: " << path << std::endl;
-  };
+  });
 
-  rorschach.on_path_erased = [](auto& path) {
+  rorschach.on(FileStatus::FILE_ERASED, [](auto &path) {
     std::cout << "Path erased: " << path << std::endl;
-  };
+  });
 
   try {
     rorschach.watch();
-  } catch (std::filesystem::filesystem_error& error) {
+  } catch (std::filesystem::filesystem_error &error) {
     std::cout << error.what() << std::endl;
   }
-
 }
