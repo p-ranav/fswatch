1423b840786d356e1c5bf779574a69fa9ce1e891
Pranav Srinivas Kumar <pranav.srinivas.kumar@gmail.com>
2019-08-25 10:39:15 -0500

    Using inotify if linux for watching files

--- include/file_watcher.hpp
+++ include/file_watcher.hpp
@@ -9,15 +9,112 @@
 #include <unordered_map>
 #include <variant>
 
-enum class FileStatus { FILE_CREATED, FILE_MODIFIED, FILE_ERASED };
+#ifdef __linux__
+#include <errno.h>
+#include <limits.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/inotify.h>
+#include <sys/types.h>
+#include <unistd.h>
+#define MAX_EVENTS 1024 /*Max. number of events to process at one go*/
+#define LEN_NAME                                                               \
+  16 /*Assuming that the length of the filename won't exceed 16 bytes*/
+#define EVENT_SIZE (sizeof(struct inotify_event)) /*size of one event*/
+#define EVENT_BUF_LEN                                                          \
+  (MAX_EVENTS * (EVENT_SIZE + LEN_NAME)) /*buffer to store the data of         \
+                                            events*/
+#define WATCH_FLAGS (IN_CREATE | IN_MODIFY | IN_DELETE)
+
+// Keep going  while run == true, or, in other words, until user hits ctrl-c
+static bool run = true;
+
+void sig_callback(int sig) { run = false; }
+
+// Watch class keeps track of watch descriptors (wd), parent watch descriptors
+// (pd), and names (from event->name). The class provides some helpers for
+// inotify, primarily to enable recursive monitoring:
+// 1. To add a watch (inotify_add_watch), a complete path is needed, but events
+// only provide file/dir name with no path.
+// 2. Delete events provide parent watch descriptor and file/dir name, but
+// removing the watch (infotify_rm_watch) needs a wd.
+//
+class Watch {
+  struct wd_elem {
+    int pd;
+    std::string name;
+    bool operator()(const wd_elem &l, const wd_elem &r) const {
+      return l.pd < r.pd ? true
+                         : l.pd == r.pd && l.name < r.name ? true : false;
+    }
+  };
+  std::map<int, wd_elem> watch;
+  std::map<wd_elem, int, wd_elem> rwatch;
+
+public:
+  // Insert event information, used to create new watch, into Watch object.
+  void insert(int pd, const std::string &name, int wd) {
+    wd_elem elem = {pd, name};
+    watch[wd] = elem;
+    rwatch[elem] = wd;
+  }
+  // Erase watch specified by pd (parent watch descriptor) and name from watch
+  // list. Returns full name (for display etc), and wd, which is required for
+  // inotify_rm_watch.
+  std::string erase(int pd, const std::string &name, int *wd) {
+    wd_elem pelem = {pd, name};
+    *wd = rwatch[pelem];
+    rwatch.erase(pelem);
+    const wd_elem &elem = watch[*wd];
+    std::string dir = elem.name;
+    watch.erase(*wd);
+    return dir;
+  }
+  // Given a watch descriptor, return the full directory name as string.
+  // Recurses up parent WDs to assemble name, an idea borrowed from Windows
+  // change journals.
+  std::string get(int wd) {
+    const wd_elem &elem = watch[wd];
+    return elem.pd == -1 ? elem.name : this->get(elem.pd) + "/" + elem.name;
+  }
+  // Given a parent wd and name (provided in IN_DELETE events), return the watch
+  // descriptor. Main purpose is to help remove directories from watch list.
+  int get(int pd, std::string name) {
+    wd_elem elem = {pd, name};
+    return rwatch[elem];
+  }
+  void cleanup(int fd) {
+    for (std::map<int, wd_elem>::iterator wi = watch.begin(); wi != watch.end();
+         wi++) {
+      inotify_rm_watch(fd, wi->first);
+      watch.erase(wi);
+    }
+    rwatch.clear();
+  }
+  void stats() {
+    std::cout << "number of watches=" << watch.size()
+              << " & reverse watches=" << rwatch.size() << std::endl;
+  }
+};
+#endif
 
 class FileWatcher {
 public:
+  enum class Event {
+    FILE_CREATED,
+    FILE_MODIFIED,
+    FILE_ERASED,
+    DIR_CREATED,
+    DIR_MODIFIED,
+    DIR_ERASED
+  };
+
   FileWatcher(const std::string &path,
-            std::chrono::duration<int, std::milli> period)
-      : running(true), path(expand(std::filesystem::path(path))),
+              std::chrono::duration<int, std::milli> period)
+      : path(expand(std::filesystem::absolute(std::filesystem::path(path)))),
         match_regex_provided(false), ignore_regex_provided(false),
-        ignore_permission_errors(false), period(period), lwt_map({}) {}
+        period(period) {}
 
   void match(const std::regex &match) {
     match_path = match;
@@ -29,82 +126,134 @@ public:
     ignore_regex_provided = true;
   }
 
-  void skip_permission_denied() { ignore_permission_errors = true; }
-
-  void on(const FileStatus &event,
+  void on(const Event &event,
           const std::function<void(const std::filesystem::path &)> &action) {
-    switch (event) {
-    case FileStatus::FILE_CREATED:
-      on_path_created = action;
-      break;
-    case FileStatus::FILE_MODIFIED:
-      on_path_modified = action;
-      break;
-    case FileStatus::FILE_ERASED:
-      on_path_erased = action;
-      break;
-    }
+    callbacks[event] = action;
   }
 
+#ifdef __linux__
   void start() {
-    std::filesystem::directory_options options;
-    if (ignore_permission_errors)
-      options = std::filesystem::directory_options::skip_permission_denied;
-    // Build file map for user-specified path
-    for (auto &file : std::filesystem::recursive_directory_iterator(
-             std::filesystem::absolute(path), options)) {
-      if (!should_ignore(file)) {
-        if (should_watch(file)) {
-          std::error_code ec;
-          lwt_map[file.path().string()] =
-              std::filesystem::last_write_time(file, ec);
-        }
-      }
+    // std::map used to keep track of wd (watch descriptors) and directory names
+    // As directory creation events arrive, they are added to the Watch map.
+    // Directory delete events should be (but currently aren't in this sample)
+    // handled the same way.
+    Watch watch;
+
+    // watch_set is used by select to wait until inotify returns some data to
+    // be read using non-blocking read.
+    fd_set watch_set;
+
+    char buffer[EVENT_BUF_LEN];
+    std::string current_dir, new_dir;
+    int total_file_events = 0;
+    int total_dir_events = 0;
+
+    // Call sig_callback if user hits ctrl-c
+    signal(SIGINT, sig_callback);
+
+    // creating the INOTIFY instance
+    // inotify_init1 not available with older kernels, consequently inotify
+    // reads block. inotify_init1 allows directory events to complete
+    // immediately, avoiding buffering delays. In practice, this significantly
+    // improves monotiring of newly created subdirectories.
+#ifdef IN_NONBLOCK
+    int fd = inotify_init1(IN_NONBLOCK);
+#else
+    int fd = inotify_init();
+#endif
+
+    // checking for error
+    if (fd < 0) {
+      perror("inotify_init");
     }
 
-    // Start watching files in path
-    while (running) {
-      std::this_thread::sleep_for(period);
-      auto it = lwt_map.begin();
+    // use select watch list for non-blocking inotify read
+    FD_ZERO(&watch_set);
+    FD_SET(fd, &watch_set);
 
-      // Check if the file was erased
-      while (it != lwt_map.end()) {
-        if (!std::filesystem::exists(it->first)) {
-          if (on_path_erased)
-            on_path_erased(std::filesystem::path(it->first));
-          it = lwt_map.erase(it);
-        } else {
-          it++;
-        }
+    const char *root = path.string().c_str();
+    int wd = inotify_add_watch(fd, root, WATCH_FLAGS);
+
+    // add wd and directory name to Watch map
+    watch.insert(-1, root, wd);
+
+    // Continue until run == false. See signal and sig_callback above.
+    while (run) {
+      // select waits until inotify has 1 or more events.
+      // select syntax is beyond the scope of this sample but, don't worry, the
+      // fd+1 is correct: select needs the the highest fd (+1) as the first
+      // parameter.
+      select(fd + 1, &watch_set, NULL, NULL, NULL);
+
+      // Read event(s) from non-blocking inotify fd (non-blocking specified in
+      // inotify_init1 above).
+      int length = read(fd, buffer, EVENT_BUF_LEN);
+      if (length < 0) {
+        perror("read");
       }
 
-      // Check if the file was created or modified
-      for (auto &file : std::filesystem::recursive_directory_iterator(
-               std::filesystem::absolute(path), options)) {
-        std::string filename = file.path().string();
-        // Watch this file only if it is not in the ignore list
-        if (!should_ignore(file)) {
-          if (should_watch(file)) {
-            std::error_code ec;
-            auto current_file_last_write_time =
-                std::filesystem::last_write_time(file, ec);
-            if (!contains(file.path().string())) {
-              lwt_map[file.path().string()] = current_file_last_write_time;
-              if (on_path_created)
-                on_path_created(file.path());
+      // Loop through event buffer
+      for (int i = 0; i < length;) {
+        struct inotify_event *event = (struct inotify_event *)&buffer[i];
+        // Never actually seen this
+        if (event->wd == -1) {
+          printf("Overflow\n");
+        }
+        // Never seen this either
+        if (event->mask & IN_Q_OVERFLOW) {
+          printf("Overflow\n");
+        }
+        if (event->len) {
+          if (event->mask & IN_IGNORED) {
+            printf("IN_IGNORED\n");
+          }
+          if (event->mask & IN_CREATE) {
+            current_dir = watch.get(event->wd);
+            if (event->mask & IN_ISDIR) {
+              new_dir = current_dir + "/" + event->name;
+              wd = inotify_add_watch(fd, new_dir.c_str(), WATCH_FLAGS);
+              watch.insert(event->wd, event->name, wd);
+              total_dir_events++;
+              printf("New directory %s created.\n", new_dir.c_str());
             } else {
-              if (lwt_map[file.path().string()] !=
-                  current_file_last_write_time) {
-                lwt_map[file.path().string()] = current_file_last_write_time;
-                if (on_path_modified)
-                  on_path_modified(file.path());
-              }
+              total_file_events++;
+              printf("New file %s/%s created.\n", current_dir.c_str(),
+                     event->name);
+            }
+          } else if (event->mask & IN_MODIFY) {
+            if (event->mask & IN_ISDIR)
+              printf("The directory %s was modified.\n", event->name);
+            else
+              printf("The file %s was modified with WD %d\n", event->name,
+                     event->wd);
+          } else if (event->mask & IN_DELETE) {
+            if (event->mask & IN_ISDIR) {
+              new_dir = watch.erase(event->wd, event->name, &wd);
+              inotify_rm_watch(fd, wd);
+              total_dir_events--;
+              printf("Directory %s deleted.\n", new_dir.c_str());
+            } else {
+              current_dir = watch.get(event->wd);
+              total_file_events--;
+              printf("File %s/%s deleted.\n", current_dir.c_str(), event->name);
             }
           }
         }
+        i += EVENT_SIZE + event->len;
       }
     }
+
+    // Cleanup
+    printf("cleaning up\n");
+    std::cout << "total dir events = " << total_dir_events
+              << ", total file events = " << total_file_events << std::endl;
+    watch.stats();
+    watch.cleanup(fd);
+    watch.stats();
+    close(fd);
+    fflush(stdout);
   }
+#endif 
 
 private:
   // Root directory of the file watcher
@@ -116,19 +265,9 @@ private:
   std::regex match_path, ignore_path;
   // Is a match/ignore regex provided
   bool match_regex_provided, ignore_regex_provided;
-  // Watcher options
-  bool ignore_permission_errors;
-  // Callback functions based on file status
-  std::function<void(const std::filesystem::path &)> on_path_created,
-      on_path_modified, on_path_erased;
-  // Manage status of file watcher
-  bool running = true;
-  // Dictionary that maps files with their respective last_write_time timestmaps
-  std::unordered_map<std::string, std::filesystem::file_time_type> lwt_map;
 
-  bool contains(const std::string &file) {
-    return lwt_map.find(file) != lwt_map.end();
-  }
+  // Callback functions based on file status
+  std::map<Event, std::function<void(const std::filesystem::path &)>> callbacks;
 
   std::filesystem::path expand(std::filesystem::path in) {
     const char *home = getenv("HOME");
--- test/main.cpp
+++ test/main.cpp
@@ -2,23 +2,22 @@
 #include <file_watcher.hpp>
 
 int main() {
-  auto watcher = FileWatcher("", std::chrono::milliseconds(500));
-  watcher.skip_permission_denied();
-  watcher.match(std::regex("bar.txt"));
+  auto watcher = FileWatcher("/home/pranav", std::chrono::milliseconds(500));
+  // watcher.match(std::regex("bar.txt"));
   // Only match foo.txt or bar.csv
   // watcher.match(std::regex("foo.txt|bar.csv"));
   // Ignore .ini files
   // watcher.ignore(std::regex(".*\\.ini$")); 
 
-  watcher.on(FileStatus::FILE_CREATED, [](auto &path) {
+  watcher.on(FileWatcher::Event::FILE_CREATED, [](auto &path) {
     std::cout << "Path created: " << path << std::endl;
   });
 
-  watcher.on(FileStatus::FILE_MODIFIED, [](auto &path) {
+  watcher.on(FileWatcher::Event::FILE_MODIFIED, [](auto &path) {
     std::cout << "Path modified: " << path << std::endl;
   });
 
-  watcher.on(FileStatus::FILE_ERASED, [](auto &path) {
+  watcher.on(FileWatcher::Event::FILE_ERASED, [](auto &path) {
     std::cout << "Path erased: " << path << std::endl;
   });
 
